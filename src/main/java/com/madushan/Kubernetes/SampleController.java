cd24afe80caa3e36e1759a8797f4bd698c0c8b35
Madushan97 <madushannipuna1997@gmail.com>
2025-03-08 18:28:17 +0530

    initial commit

---
+++ src/main/java/com/madushan/Kubernetes/SampleController.java
@@ -0,0 +1,15 @@
+package com.madushan.Kubernetes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class SampleController {
+
+    @GetMapping
+    public String getGreeting() {
+        return "Make sure table names (rule, rule_configuration, table_list) and column names match your DB schema.";
+    }
+}
